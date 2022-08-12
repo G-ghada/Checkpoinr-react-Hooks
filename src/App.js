69d5aaa4323c31d4935aeb350a261b6d76b39458
Ghada <bousselmig049@gmail.com>
2022-08-12 18:41:56 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import "./App.css";
+import MoviesList from "./components/MoviesList";
+import NavBar from "./components/NavBar";
+function App() {
+  const [search, setSearch] = useState("");
+  const [rating, setRating] = useState();
+  console.log("rating: ", rating);
+  return (
+    <div className="App">
+      <NavBar setSearch={setSearch} setRating={setRating} rating={rating} />
+      <MoviesList search={search} rating={rating} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,81 @@
+export const data = [
+  {
+    title: "Blanche-Neige et les Sept Nains",
+    category: "Cartoon",
+    id: Math.random(),
+    rate: 8,
+    cover:
+      "https://i-mom.unimedias.fr/2020/09/16/blanche-neige-et-les-sept-nains.jpg?auto=format,compress&cs=tinysrgb",
+    realisateur:
+      "David Hand, Perce Pearce, Ben Sharpsteen, Larry Morey, Wilfred Jackson, Bill Cottrell",
+    release: "4 février 1938",
+  },
+  {
+    title: "La Petite Sirène",
+    category: "Cartoon",
+    id: Math.random(),
+    rate: 7,
+    cover:
+      "https://i0.wp.com/www.filmspourenfants.net/wp-content/uploads/2018/05/la-petite-sirene-a.jpg?fit=555%2C754&ssl=1",
+    realisateur: "John Musker, Ron Clements",
+    release: "13 novembre 1989 ",
+  },
+  {
+    title: "La Belle et la Bête",
+    category: "Cartoon",
+    id: Math.random(),
+    rate: 7,
+    cover:
+      "https://img2.cdn.cinoche.com/images/b21edc2b267299c09ec7335896623f3a.jpg",
+    realisateur: "Gary Trousdale, Kirk Wise",
+    release: " 22 novembre 1991",
+  },
+  {
+    title: "La Princesse et la Grenouille",
+    category: "Cartoon",
+    id: Math.random(),
+    rate: 8,
+    cover:
+      "https://play-lh.googleusercontent.com/-6dL8OmKQ8F8FVZ1EnnAFulmit-gLv6cugcHcUgLvWOwKYk7GC-KAQPeuGImjMg2GCet-w",
+    realisateur: " John Musker, Ron Clements",
+    release: "11 décembre 2009",
+  },
+  {
+    title: "Aladdin",
+    category: "Cartoon",
+    id: Math.random(),
+    rate: 9,
+    cover:
+      "https://www.ecranlarge.com/uploads/image/001/194/climg673iaslbmvz9t5ln2o3qbg-208.jpg",
+    realisateur: " Ron Clements, John Musker",
+    release: "25 novembre 1992",
+  },
+  {
+    title: "Hercule",
+    category: "Cartoon",
+    id: Math.random(),
+    rate: 9,
+    cover: "https://fr.web.img6.acsta.net/pictures/20/03/18/15/40/3355358.jpg",
+    realisateur: "Ron Clements, John Musker",
+    release: "13 juin 1997",
+  },
+  {
+    title: "Cendrillon",
+    category: "Cartoon",
+    id: Math.random(),
+    rate: 6,
+    cover:
+      "https://i0.wp.com/www.filmspourenfants.net/wp-content/uploads/2018/03/cendrillon-a.jpg?fit=473%2C640&ssl=1",
+    realisateur: "Clyde Geronimi, Hamilton Luske, Wilfred Jackson",
+    release: "4 mars 1950",
+  },
+  {
+    title: "Pocahontas",
+    category: "Cartoon",
+    id: Math.random(),
+    rate: 8,
+    cover: "https://fr.web.img4.acsta.net/pictures/17/07/10/16/07/137739.jpg",
+    realisateur: "Eric Goldberg, Mike Gabriel",
+    release: "23 juin 1995",
+  },
+];
